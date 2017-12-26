98e5d9a92caf9abe4571a32de657005d2b3017fd
Helen Liutongco <bellpickle@gmail.com>
2017-12-26 22:12:20 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 #The parrot method takes the phrase argument, which has the default value of "Squawk!"
-#It puts the phrase and then returns the phrase
+#It outputs the phrase and then returns the phrase
 
 def parrot(phrase="Squawk!")
   puts phrase
